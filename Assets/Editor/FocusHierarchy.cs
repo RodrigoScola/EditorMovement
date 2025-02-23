e4051cda9476e30d56dbda9eb60ea88b5a1c5724
rodrigo <rodrigo.sgarabotto.scola@gmail.com>
2025-02-23 18:17:39 -0300

    initial commit

---
+++ Assets/Editor/FocusHierarchy.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Focus
+{
+    public static class Hierarchy
+    {
+        private static Transform lastSelected;
+
+        public static bool IsHierarchy(EditorWindow window)
+        {
+            Type hierarchyType = Type.GetType("UnityEditor.SceneHierarchyWindow, UnityEditor");
+
+            return window is null || hierarchyType != window.GetType() ? false : true;
+        }
+
+        static Transform GetLastSelected()
+        {
+            if (lastSelected is not null)
+            {
+                return lastSelected;
+            }
+
+            return FocusComponents.GetRootObjects().FirstOrDefault();
+        }
+
+        public static void Down()
+        {
+            var components = Selection.activeGameObject;
+            components ??= GetLastSelected()?.gameObject;
+
+            var nextComponent = FocusComponents.PreviousComponent(components.transform);
+
+            nextComponent ??= components.transform.GetChild(0);
+
+            if (nextComponent is not null)
+            {
+                Selection.activeGameObject = nextComponent.gameObject;
+                lastSelected = nextComponent;
+            }
+        }
+
+        public static void Up()
+        {
+            var current = Selection.activeGameObject.transform;
+
+            var nextComponent = FocusComponents.NextComponent(current);
+
+            nextComponent ??= current.parent.transform;
+
+            if (nextComponent is not null)
+            {
+                Selection.activeGameObject = nextComponent.gameObject;
+                lastSelected = nextComponent;
+            }
+        }
+
+        public static void Left()
+        {
+            var current = Selection.activeGameObject.transform;
+
+            if (current.parent is not null)
+            {
+                Selection.activeGameObject = current.parent.gameObject;
+                lastSelected = current.parent;
+            }
+        }
+
+        public static void Right()
+        {
+            if (FocusWindow.IsExpanded(Selection.activeGameObject))
+            {
+                EditorWindow.focusedWindow.SendEvent(Event.KeyboardEvent("left"));
+            }
+            else
+            {
+                var e = Event.KeyboardEvent("right");
+                EditorWindow.focusedWindow.SendEvent(e);
+            }
+        }
+    }
+}
